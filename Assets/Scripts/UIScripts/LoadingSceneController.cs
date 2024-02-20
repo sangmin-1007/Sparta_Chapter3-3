56641367aa81e0a73a79df341bbe2b9f988964e5
sangmin-1007 <dosin0822@gmail.com>
2024-02-20 19:50:31 +0900

    Feat : LoadingSceneController.cs 추가
    로딩씬을 불러오고 다음씬을 비동기로 준비하기 위한 LoadingSceneController.cs 추가
    
    <사용법>
    LoadingSceneController.LoadScene("씬 이름");

---
+++ Assets/Scripts/UIScripts/LoadingSceneController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingSceneController : MonoBehaviour
+{
+    static string nextScene;
+
+    [Header("бс Image")]
+    [SerializeField] private Image loadingBar;
+
+    [Header("бс Text")]
+    [SerializeField] private string[] tip;
+    [SerializeField] private TextMeshProUGUI tipText;
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("LoadingScene");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneProcess());
+        int randomIndex = Random.Range(0, tip.Length);
+        tipText.text = tip[randomIndex];
+    }
+
+
+    private IEnumerator LoadSceneProcess()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0f;
+
+        while (!op.isDone)
+        {
+            yield return null;
+
+            if (op.progress < 0.9f)
+            {
+                loadingBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                loadingBar.fillAmount = Mathf.Lerp(0.9f, 1f, timer * 0.5f);
+                if (loadingBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+
+    }
+}
