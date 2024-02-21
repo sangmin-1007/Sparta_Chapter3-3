4ccec8a2adda8d2e8ce2fa06ceb2146a93e1a7d5
sangmin-1007 <dosin0822@gmail.com>
2024-02-21 16:28:05 +0900

    Feat : 씬에 따라 BGM 전환 구현

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,13 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class AudioData : SerializableDictionary<string, AudioClip> { }
 public class AudioManager : MonoBehaviour
 {
-    private static AudioManager instance;
-    public static AudioManager Instance => instance;
+    private static AudioManager Instance;
+    public static AudioManager instance => Instance;
 
     [Header("бс AudioData")]
     public AudioData audioData;
@@ -15,12 +17,11 @@ public class AudioManager : MonoBehaviour
     [Header("бс AudioSource")]
     [SerializeField] private AudioSource audioSource;
 
-
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
-            instance = this;
+            Instance = this;
         }
         else
         {
@@ -30,11 +31,46 @@ public class AudioManager : MonoBehaviour
         DontDestroyOnLoad(gameObject);
     }
 
-    public void OnPlayBGM(string bgmName)
+    private void Start()
     {
+        OnLoadStartScene("StartSceneBgm");
+        SceneManager.sceneLoaded += OnLoadedSceneEvent;
+    }
+
+    public void OnLoadStartScene(string bgmName)
+    {
+        if (SceneManager.GetActiveScene().name != "StartScene")
+            return;
+
         audioSource.clip = audioData[bgmName];
         audioSource.Play();
         audioSource.loop = true;
+    }
+
+    public void OnLoadGameScene(string bgmName)
+    {
+        if (SceneManager.GetActiveScene().name != "GameScene")
+            return;
+
+        audioSource.clip = audioData[bgmName];
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    public void StopBgm()
+    {
+        audioSource.Stop();
+    }
+
+    public void PlayBgm()
+    {
+        audioSource.Play();
+    }
+
+    public void OnLoadedSceneEvent(Scene scene,LoadSceneMode mode)
+    {
+        OnLoadGameScene("GameSceneBgm");
+        OnLoadStartScene("StartSceneBgm");
     }
 
 }
