e51ff4400a4740c302957cc1d5db7554f904f538
sangmin-1007 <dosin0822@gmail.com>
2024-02-21 12:58:27 +0900

    Feat : Player 캐릭터의 이동 구현

---
+++ Assets/Inputs/InputController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public event Action<Vector2> OnMoveEvent;
+    public event Action<Vector2> OnLookEvent;
+
+    protected SpriteRenderer _spriteRenderer;
+
+    public void CallMoveEvent(Vector2 dir)
+    {
+        OnMoveEvent?.Invoke(dir);
+    }
+
+    public void CallLookEvent(Vector2 dir)
+    {
+        if(dir.x <= 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _spriteRenderer.flipX = false;
+        }
+
+        OnLookEvent?.Invoke(dir);
+    }
+
+
+}
---
+++ Assets/Inputs/PlayerMoveMent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveMent : MonoBehaviour
+{
+    private InputController _controller;
+
+    private Vector2 _moveDir = Vector2.zero;
+    private Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _controller = GetComponent<InputController>();
+        _rb = GetComponentInChildren<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        _controller.OnMoveEvent += Move;
+
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement(_moveDir);
+    }
+
+    private void Move(Vector2 dir)
+    {
+        _moveDir = dir;
+    }
+
+    private void ApplyMovement(Vector2 dir)
+    {
+        dir = dir * 5;
+
+        _rb.velocity = dir;
+    }
+}
